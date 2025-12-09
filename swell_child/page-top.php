0827ee43ed098718a9f5d43955e68119e97e413f
soramame0925 <sora1968924391014@gmail.com>
2025-12-09 20:02:27 +0900

    Add files via upload

--- swell_child/page-top.php
+++ swell_child/page-top.php
@@ -2,23 +2,26 @@
 /*
 Template Name: トップページ（最新一覧）
 */
-get_header();
+get_header(); ?>
 
-$query = new WP_Query( [
-  'post_type'      => 'post',
-  'posts_per_page' => 12,
-  'orderby'        => 'date',
-  'order'          => 'DESC',
-] );
+<main class="mno-container">
+  <?php
+  $query = new WP_Query([
+    'post_type' => 'post',
+    'posts_per_page' => 12,
+    'orderby' => 'date',
+    'order' => 'DESC',
+  ]);
 
-if ( $query->have_posts() ) :
-  echo '<div class="mno-grid">';
-  while ( $query->have_posts() ) :
-    $query->the_post();
-    get_template_part( 'template-parts/card' );
-  endwhile;
-  echo '</div>';
-  wp_reset_postdata();
-endif;
+  if($query->have_posts()):
+    echo '<div class="mno-grid">';
+    while($query->have_posts()): $query->the_post();
+      get_template_part('template-parts/card');
+    endwhile;
+    echo '</div>';
+    wp_reset_postdata();
+  endif;
+  ?>
+</main>
 
-get_footer();
+<?php get_footer(); ?>
--- swell_child/single.php
+++ swell_child/single.php
@@ -1,67 +1,72 @@
 <?php get_header(); ?>
 
-<?php
-if ( have_posts() ) :
-  while ( have_posts() ) : the_post();
-    $custom_title = get_post_meta( get_the_ID(), '_mpm_custom_title', true );
-    $display_title = '' !== $custom_title ? $custom_title : get_the_title();
-    $title_markup  = '<div class="mno-title-block">';
-    $title_markup .= '<h1 class="mno-single-title">' . esc_html( $display_title ) . '</h1>';
-    $title_markup .= '</div>';
-
-    if ( function_exists( 'mno_pm_render_single_template' ) ) {
-      $single_content = mno_pm_render_single_template( get_the_ID() );
-
-      if ( false !== strpos( $single_content, 'mno-pm-article__gallery' ) ) {
-        $original_content = $single_content;
-
-        $single_content = preg_replace(
-          '/(<div\s+class="mno-pm-article__gallery"[^>]*>.*?<\/div>)/s',
-           '$1' . $title_markup,
-          $single_content,
-          1
-        );
-
-        if ( null === $single_content ) {
-           $single_content = $original_content . $title_markup;
-        }
-      } else {
-        $single_content = $title_markup . $single_content;
-      }
-
-      echo $single_content;
-    } else {
-      echo $title_markup;
-      $voice_sample_value = get_post_meta( get_the_ID(), '_mpm_voice_sample', true );
-      if ( $voice_sample_value ) {
-        $allowed_tags = wp_kses_allowed_html( 'post' );
-        $allowed_tags['iframe'] = [
-          'src'             => true,
-          'width'           => true,
-          'height'          => true,
-          'frameborder'     => true,
-          'allow'           => true,
-          'allowfullscreen' => true,
-          'loading'         => true,
-          'title'           => true,
-          'referrerpolicy'  => true,
-        ];
-
-        $voice_sample_html = wp_kses( $voice_sample_value, $allowed_tags );
-
-        if ( $voice_sample_html ) {
-          echo '<div class="mno-voice-sample">' . $voice_sample_html . '</div>';
-        }
-      }
-      echo '<div class="mno-single__content">' . apply_filters( 'the_content', get_the_content() ) . '</div>';
-    }
-  endwhile;
-else :
-  ?>
-  <p>記事が見つかりませんでした。</p>
+<main class="mno-container">
   <?php
-endif;
-?>
+  if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+      <article <?php post_class('mno-single'); ?>>
+        <?php
+        $custom_title = get_post_meta( get_the_ID(), '_mpm_custom_title', true );
+        $display_title = '' !== $custom_title ? $custom_title : get_the_title();
+        $title_markup  = '<section class="mno-pm-article__section mno-title-block">';
+        $title_markup .= '<h1 class="mno-single-title">' . esc_html( $display_title ) . '</h1>';
+        $title_markup .= '</section>';
+
+        if ( function_exists( 'mno_pm_render_single_template' ) ) {
+          $single_content = mno_pm_render_single_template( get_the_ID() );
+
+          if ( false !== strpos( $single_content, 'mno-pm-article__gallery' ) ) {
+            $original_content = $single_content;
+
+            $single_content = preg_replace(
+              '/(<section\s+class="mno-pm-article__section\s+mno-pm-article__gallery"[^>]*>.*?<\/section>)/s',
+               '$1' . $title_markup,
+              $single_content,
+              1
+            );
+
+            if ( null === $single_content ) {
+               $single_content = $original_content . $title_markup;
+            }
+          } else {
+            $single_content = $title_markup . $single_content;
+          }
+
+          echo $single_content;
+        } else {
+          echo $title_markup;
+          $voice_sample_value = get_post_meta( get_the_ID(), '_mpm_voice_sample', true );
+          if ( $voice_sample_value ) {
+            $allowed_tags = wp_kses_allowed_html( 'post' );
+            $allowed_tags['iframe'] = [
+              'src'             => true,
+              'width'           => true,
+              'height'          => true,
+              'frameborder'     => true,
+              'allow'           => true,
+              'allowfullscreen' => true,
+              'loading'         => true,
+              'title'           => true,
+              'referrerpolicy'  => true,
+            ];
+
+            $voice_sample_html = wp_kses( $voice_sample_value, $allowed_tags );
+
+            if ( $voice_sample_html ) {
+              echo '<div class="mno-voice-sample">' . $voice_sample_html . '</div>';
+            }
+          }
+          echo '<div class="mno-single__content">' . apply_filters( 'the_content', get_the_content() ) . '</div>';
+        }
+        ?>
+      </article>
+
+    <?php endwhile;
+  else : ?>
+    <p>記事が見つかりませんでした。</p>
+  <?php endif; ?>
+</main>
 
 <div class="mno-gallery-lightbox" data-mno-gallery-lightbox hidden aria-hidden="true" role="dialog" aria-modal="true">
   <figure class="mno-gallery-lightbox__figure">
