1a0b022f2583f6b60d9119ef7c0e5eb860ffd896
soramame0925 <sora1968924391014@gmail.com>
2025-12-11 04:02:49 +0900

    Delete swell_child directory

--- swell_child/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-    return;
-}
-
-// 追加ファイルの読み込み。
-require_once get_stylesheet_directory() . '/inc/enqueue.php';
-require_once get_stylesheet_directory() . '/inc/custom-post-types.php';
